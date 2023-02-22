b8c989eab4f1ee6dc17c2c31447c765a3ccb4016
Irene Brignoli <ire.brignoli@gmail.com>
2023-02-22 17:20:53 +0100

    js sistemato

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-/*
-
-Ciao ragazzi,
-esercizio di oggi: calcolo del prezzo del biglietto del treno
-cartella/repo js-biglietto-treno
-Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-
-
-n kilometri che vuole fare
-età passeggero
-
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio
-
-
-*/
 
 
 
@@ -30,6 +10,7 @@ console.log('Età: ' + eta);
 let prezzoAlKm = 0.21;
 
 let prezzoBigliettoBase = prezzoAlKm * distanza;
+prezzoBigliettoBase = prezzoBigliettoBase.toFixed(2);
 console.log('Prezzo biglietto normale: ' + prezzoBigliettoBase);
 
 let scontoGiovani = (prezzoBigliettoBase / 100) * 20;
@@ -38,8 +19,25 @@ console.log('Sconto giovani: ' + scontoGiovani);
 let scontoAnziani = (prezzoBigliettoBase / 100) * 40;
 console.log('Sconto anziani: ' + scontoAnziani);
 
-let risultato;
+let bigliettoGiovani = prezzoBigliettoBase - scontoGiovani;
+bigliettoGiovani = bigliettoGiovani.toFixed(2);
 
+let bigliettoAnziani = prezzoBigliettoBase - scontoAnziani;
+bigliettoAnziani = bigliettoAnziani.toFixed(2);
+
+
+if (eta < 18) {
+  document.getElementById('prezzo-viaggio').innerHTML = bigliettoGiovani + '€ Hai uno sconto del 20% perché sei minorenne!';
+  
+} else if (eta > 65) {
+  document.getElementById('prezzo-viaggio').innerHTML = bigliettoAnziani + '€ Hai uno sconto del 40% perché sei over 65!';
+
+} else {
+  document.getElementById('prezzo-viaggio').innerHTML = prezzoBigliettoBase + '€';
+ 
+}
+
+/*
 if (eta < 18) {
   risultato = prezzoBigliettoBase - scontoGiovani;
 } else if (eta > 65) {
@@ -47,7 +45,4 @@ if (eta < 18) {
 } else {
   risultato = prezzoBigliettoBase;
 }
-
-risultato = risultato.toFixed(2);
-
-document.getElementById('prezzo-viaggio').innerHTML = risultato;
+*/
